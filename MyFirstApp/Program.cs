542dee602b0fa32f1fece14c072238ecdb925557
Aleks Lemming <aleks.lemming@tktk.ee>
2023-01-26 11:39:56 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajalt oma nimi
+//rakendus tervitab kasutajat nimepidi
+
+
+Console.WriteLine("Enter your name:");
+string userName = Console.ReadLine();
+
+
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+Console.WriteLine($"Hello, {userName}!");
